39109a330c70d95cb4ff0cfa3a23ecb8db99f203
Robin Kalleicher <robin.kalleicherqaware.de>
2022-06-03 16:30:10 +0200

    implementing docker-compose for multi container application including databases

--- applications/SpringBootWeather/src/main/java/de/qaware/springbootweather/SpringBootWeatherApplication.java
+++ applications/SpringBootWeather/src/main/java/de/qaware/springbootweather/SpringBootWeatherApplication.java
@@ -2,9 +2,6 @@ package de.qaware.springbootweather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.util.logging.Logger;
 
 @SpringBootApplication
 public class SpringBootWeatherApplication {
